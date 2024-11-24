3d19793d2a5e7743b0d8e0af7cd36d77a94ac084
Alexandros Soumpasis <160520101+alexsoump@users.noreply.github.com>
2024-11-24 15:50:09 +0200

    bind 1.0
    eftiaksa mia endeiktikh bind()

--- lib/microtcp.c
+++ lib/microtcp.c
@@ -31,7 +31,37 @@ int
 microtcp_bind (microtcp_sock_t *socket, const struct sockaddr *address,
                socklen_t address_len)
 {
-  /* Your code here */
+  if(socket == NULL || address == NULL || address_len < sizeof(struct sockaddr_in)){
+    fprintf(stderr, "Invalid arguments to microtcp_bind\n");
+    return -1;
+  }
+
+  // cast sockaddr_in to work with IPv4
+  const struct sockaddr_in *addr_in = (const struct sockaddr_in *)address;
+
+  if(socket->state != INVALID){
+    fprintf(stderr, "Socket is in an invalid state\n");
+    return -1;
+  }
+  
+  if (bind(socket->sd, address, address_len) < 0) {
+    perror("bind failed in microtcp_bind");
+    return -1;
+  }
+
+  socket->state = LISTEN; 
+
+  /* inet_ntoa: internet address to ASCII, converts IP address from binary, to human readable
+  * ntohs: network to host short, converts a 16-bit from big-endian to host byte order
+  */ 
+
+  printf("microTCP socket bound to IP: %s, Port: %d\n", 
+          inet_ntoa(addr_in->sin_addr), ntohs(addr_in->sin_port));
+  return 0;  // success
+
+    
+
+
 }
 
 int
